f539faf5906535b12ae4c1086b44bd1bf81011d0
tommy-hartanto <107762612+tommy-hartanto@users.noreply.github.com>
2022-10-18 20:02:46 +0700

    another minor fix

--- queries.sql
+++ queries.sql
@@ -1,4 +1,3 @@
-TUGAS 1
 -- Stage 1:
 -- 	- Assign primary keys and foreign keys
 
@@ -141,7 +140,7 @@ REFERENCES customers_dataset (customer_id);
 
 
 
-TUGAS 2
+
 
 -- Stage 2:
 -- 1. Average of monthly active users (MAU)
@@ -152,32 +151,31 @@ TUGAS 2
 -- These are how each one of those is queried:
 
 CREATE TABLE stage_2 (
-    year INT,
+	year INT,
 	mau_avg DOUBLE PRECISION,
 	customers_new INT,
 	customers_repeat_order INT,
-	customers_order_average INT
+	customers_order_average DOUBLE PRECISION
 	);
 
 
 -- 1. Average of monthly active users (MUA):
 
-INSERT INTO 
-    stage_2 (mau_avg, year) -- Insert results into table step_2
+INSERT INTO stage_2 (mau_avg, year) -- Insert results into table step_2
+SELECT
+AVG(customer_count) AS customer_count_avg, -- Average MAU
+purchase_year -- Only select the year
+FROM(
 	SELECT
-		AVG(customer_count) AS customer_count_avg, -- Average MAU
-		purchase_year -- Only select the year
-		FROM(
-		SELECT
-			COUNT(DISTINCT cd.customer_unique_id) AS customer_count, -- MAU
-			DATE_PART ('month', od.order_purchase_timestamp) AS purchase_month, -- Month
-			DATE_PART ('year', od.order_purchase_timestamp) AS purchase_year -- Year
-			FROM orders_dataset od
-			JOIN customers_dataset cd
-			ON od.customer_id = cd.customer_id
-			GROUP BY purchase_year, purchase_month -- Group by month and year
-		) AS t1
-		GROUP BY purchase_year; -- MUA averaged by year
+	COUNT(DISTINCT cd.customer_unique_id) AS customer_count, -- MAU
+	DATE_PART ('month', od.order_purchase_timestamp) AS purchase_month, -- Month
+	DATE_PART ('year', od.order_purchase_timestamp) AS purchase_year -- Year
+	FROM orders_dataset od
+	JOIN customers_dataset cd
+	ON od.customer_id = cd.customer_id
+	GROUP BY purchase_year, purchase_month -- Group by month and year
+) AS t1
+GROUP BY purchase_year; -- MUA averaged by year
 
 	
 -- 2. Number of new users:
@@ -187,7 +185,7 @@ SET customers_new = order_count_sum
 FROM(
 	SELECT
 	purchase_year_first,
-	COUNT(1)
+	COUNT(1) AS order_count_sum
 	FROM (
 		SELECT
 		DISTINCT cd.customer_unique_id, -- Unique users
@@ -198,59 +196,57 @@ FROM(
 		GROUP BY cd.customer_unique_id -- How many order(s) each user made
 	) AS t1
 	GROUP BY purchase_year_first
-	ORDER BY purchase_year_first
+	ORDER BY purchase_year_first) AS t2
+	WHERE stage_2.year = t2.purchase_year_first;
 
 	
 -- 3. Number of users with repeat order:
 
 UPDATE stage_2
 SET customers_repeat_order = order_count_sum
-	FROM(
+FROM(
+	SELECT
+	COUNT(order_count) AS order_count_sum, -- Total the number of orders
+	purchase_year -- Year
+	FROM (
 		SELECT
-			COUNT(order_count) AS order_count_sum, -- Total the number of orders
-			purchase_year -- Year
-			FROM (
-				SELECT
-					DISTINCT customer_unique_id AS unique_id, -- Unique users
-					COUNT(order_id) AS order_count, -- Number of orders
-					DATE_PART('year', order_purchase_timestamp) AS purchase_year
-					FROM orders_dataset AS od
-					JOIN customers_dataset AS cd
-						ON od.customer_id = cd.customer_id
-					GROUP BY unique_id, purchase_year -- How many order(s) each user made in each year
-				) AS t1
-			WHERE order_count > 1 -- How many users with two or more orders
-			GROUP BY purchase_year -- How many of these users per year
-		) t2
-		WHERE stage_2.year = t2.purchase_year;
+		DISTINCT customer_unique_id AS unique_id, -- Unique users
+		COUNT(order_id) AS order_count, -- Number of orders
+		DATE_PART('year', order_purchase_timestamp) AS purchase_year
+		FROM orders_dataset AS od
+		JOIN customers_dataset AS cd
+		ON od.customer_id = cd.customer_id
+		GROUP BY unique_id, purchase_year -- How many order(s) each user made in each year
+	) AS t1
+	WHERE order_count > 1 -- How many users with two or more orders
+	GROUP BY purchase_year -- How many of these users per year
+) AS t2
+WHERE stage_2.year = t2.purchase_year;
 
 
 -- 4. Average number of orders:
 
 UPDATE stage_2
 SET customers_order_average = order_count_sum
+FROM(
+	SELECT
+	AVG(order_count) AS order_count_sum, -- Average number of orders per customer
+	purchase_year
 	FROM(
 		SELECT
-			AVG(order_count) AS order_count_sum, -- Average number of orders per customer
-			purchase_year
-			FROM(
-			SELECT
-				DISTINCT customer_unique_id AS unique_id,
-				COUNT(order_id) AS order_count, -- Number of orders
-				DATE_PART('year', order_purchase_timestamp) AS purchase_year -- Year
-				FROM orders_dataset AS od
-				JOIN customers_dataset AS cd
-					ON od.customer_id = cd.customer_id
-				GROUP BY purchase_year, unique_id -- Number of orders in each year
-				) t1
-			GROUP BY purchase_year -- Average number of orders per customer per year
-		) t2
-	WHERE stage_2.year = t2.purchase_year;
+		DISTINCT customer_unique_id AS unique_id,
+		COUNT(order_id) AS order_count, -- Number of orders
+		DATE_PART('year', order_purchase_timestamp) AS purchase_year -- Year
+		FROM orders_dataset AS od
+		JOIN customers_dataset AS cd
+		ON od.customer_id = cd.customer_id
+		GROUP BY purchase_year, unique_id -- Number of orders in each year
+		) AS t1
+	GROUP BY purchase_year -- Average number of orders per customer per year
+) AS t2
+WHERE stage_2.year = t2.purchase_year;
 
-SELECT *
-	FROM stage_2
 
-TUGAS 3
 
 -- Stage 3:
 -- 1. Annual revenue
@@ -264,111 +260,114 @@ CREATE TABLE stage_3 ( -- create table with the name 'stage_3' to contain future
 	revenue DOUBLE PRECISION, -- annual revenue
 	order_cancelled INT, -- number of cancelled orders per year
 	revenue_highest_product_category VARCHAR, -- product category with the highest revenue in the year
-	order_cancelled_highest_product_category VARCHAR) -- product category with the most canceled order in the year
+	order_cancelled_highest_product_category VARCHAR); -- product category with the most canceled order in the year
 
 
 -- 1.
 
 INSERT INTO stage_3 (revenue, year) -- insert values to the columns in assigned table
 SELECT
-	SUM(price + freight_value) AS revenue, -- sum revenue
-	purchase_year
+SUM(price + freight_value) AS revenue, -- sum revenue
+purchase_year
 FROM (
 	SELECT
-		price, -- item price
-		freight_value, -- cost of shipping
-		DATE_PART ('year', od.order_purchase_timestamp) AS purchase_year -- year
+	price, -- item price
+	freight_value, -- cost of shipping
+	DATE_PART ('year', od.order_purchase_timestamp) AS purchase_year -- year
 	FROM order_items_dataset AS oid
 	JOIN orders_dataset AS od
-		ON oid.order_id = od.order_id
-	WHERE od.order_status != 'canceled' -- filter for all orders but canceled ones
-	) AS t1
+	ON oid.order_id = od.order_id
+	WHERE od.order_status = 'delivered' -- look for delivered orders only
+) AS t1
 GROUP BY t1.purchase_year -- aggregate based on year
-ORDER BY t1.purchase_year -- make sure the year is in logical order
+ORDER BY t1.purchase_year; -- make sure the year is in logical order
 
 
 -- 2.
 
 UPDATE stage_3
-	SET order_cancelled = canceled_count -- update assigned table
+SET order_cancelled = canceled_count -- update assigned table
 FROM (
 	SELECT
-		COUNT(*) AS canceled_count, -- count canceled count
-		DATE_PART('year', od.order_purchase_timestamp) AS purchase_year -- year
+	COUNT(*) AS canceled_count, -- count canceled count
+	DATE_PART('year', od.order_purchase_timestamp) AS purchase_year -- year
 	FROM orders_dataset AS od
 	WHERE od.order_status = 'canceled'
 	GROUP BY purchase_year -- aggregate based on year
-	) AS t1
-WHERE stage_3.year = t1.purchase_year
+) AS t1
+WHERE stage_3.year = t1.purchase_year;
 
 
 -- 3.
 
 UPDATE stage_3
-	SET revenue_highest_product_category = product_category_name -- update assigned table
+SET revenue_highest_product_category = product_category_name -- update assigned table
 FROM (
 	SELECT
-		purchase_year,
-		product_category_name,
-		rank -- third, show only the highest ranked
+	purchase_year,
+	product_category_name,
+	rank -- third, show only the highest ranked
 	FROM (
 		SELECT
-			purchase_year,
-			product_category_name,
-			rank() OVER (PARTITION BY purchase_year ORDER BY revenue DESC) -- second, rank annual revenue
+		purchase_year,
+		product_category_name,
+		rank() OVER (PARTITION BY purchase_year ORDER BY revenue DESC), -- second, rank annual revenue
+		revenue
 		FROM (
 			SELECT
-				DATE_PART('year', order_purchase_timestamp) AS purchase_year, -- year
-				product_category_name, -- product category
-				(price + freight_value) AS revenue -- first, create new column revenue
+			DATE_PART('year', order_purchase_timestamp) AS purchase_year, -- year
+			product_category_name, -- product category
+			SUM(price + freight_value) AS revenue -- first, create new column revenue
 			FROM orders_dataset AS od
 			JOIN order_items_dataset AS oid
-				ON od.order_id = oid.order_id
+			ON od.order_id = oid.order_id
 			JOIN product_dataset AS pd
-				ON oid.product_id = pd.product_id
-			) AS t1
-		) AS t2
+			ON oid.product_id = pd.product_id
+			WHERE od.order_status = 'delivered'
+			GROUP BY purchase_year, product_category_name
+		) AS t1
+	) AS t2
 	WHERE rank = 1 -- filter for only the highest rank of revenue
 	GROUP BY purchase_year, product_category_name, rank -- group based on year and product category respectively
-	) AS t3
-WHERE stage_3.year = t3.purchase_year
+) AS t3
+WHERE stage_3.year = t3.purchase_year;
 
 
 -- 4.
 
 UPDATE stage_3
-	SET order_cancelled_highest_product_category = product_category_name -- update assigned table
+SET order_cancelled_highest_product_category = product_category_name -- update assigned table
 FROM (
 	SELECT
-		purchase_year,
-		product_category_name,
-		rank -- third, show only the highest ranked
+	purchase_year,
+	product_category_name,
+	rank -- third, show only the highest ranked
 	FROM (
 		SELECT
-			purchase_year,
-			product_category_name,
-			rank() OVER (PARTITION BY purchase_year ORDER BY order_canceled_count DESC) -- second, rank counted canceled order
+		purchase_year,
+		product_category_name,
+		rank() OVER (PARTITION BY purchase_year ORDER BY order_canceled_count DESC) -- second, rank counted canceled order
 		FROM (
 			SELECT
-				DATE_PART('year', order_purchase_timestamp) AS purchase_year, -- year
-				product_category_name, -- product category
-				COUNT(order_status) AS order_canceled_count -- first, count canceled order
+			DATE_PART('year', order_purchase_timestamp) AS purchase_year, -- year
+			product_category_name, -- product category
+			COUNT(order_status) AS order_canceled_count -- first, count canceled order
 			FROM orders_dataset AS od
 			JOIN order_items_dataset AS oid
-				ON od.order_id = oid.order_id
+			ON od.order_id = oid.order_id
 			JOIN product_dataset AS pd
-				ON oid.product_id = pd.product_id
+			ON oid.product_id = pd.product_id
 			WHERE order_status = 'canceled'
 			GROUP BY purchase_year, product_category_name -- aggregate based on year and product category
-			) AS t1
-		) AS t2
+		) AS t1
+	) AS t2
 	WHERE rank = 1 -- filter for only the highest rank of counted canceled order
 	GROUP BY purchase_year, product_category_name, rank -- group based on year and product category respectively
-	) AS t3
-WHERE stage_3.year = t3.purchase_year
+) AS t3
+WHERE stage_3.year = t3.purchase_year;
 
 
-TUGAS 4
+
 
 -- Stage 4:
 -- 	- Usage Frequency Per Payment Type
@@ -387,20 +386,20 @@ CREATE TABLE stage_4 (
 
 INSERT INTO stage_4 (payment_type, payment_type_frequency) -- insert the values into the respective table
 SELECT
-	payment_type,
-	SUM(payment_type_count) AS payment_type_sum -- sum to get how many times a payment type was used
+payment_type,
+SUM(payment_type_count) AS payment_type_sum -- sum to get how many times a payment type was used
 FROM (
 	SELECT
-		payment_type, -- payment type
-		CASE payment_type -- assign 1 for summing up later
-			WHEN 'not_defined' THEN 1
-			WHEN 'boleto' THEN 1
-			WHEN 'debit_card' THEN 1
-			WHEN 'voucher' THEN 1
-			WHEN 'credit_card' THEN 1
-		END AS payment_type_count
+	payment_type, -- payment type
+	CASE payment_type -- assign 1 for summing up later
+		WHEN 'not_defined' THEN 1
+		WHEN 'boleto' THEN 1
+		WHEN 'debit_card' THEN 1
+		WHEN 'voucher' THEN 1
+		WHEN 'credit_card' THEN 1
+	END AS payment_type_count
 	FROM order_payments_dataset
-	) AS t1
+) AS t1
 GROUP BY payment_type -- aggregate based on payment type
 ORDER BY payment_type_sum DESC; -- set in logical order
 
@@ -410,103 +409,100 @@ ORDER BY payment_type_sum DESC; -- set in logical order
 -- Year 2016
 
 UPDATE stage_4 -- insert the values into the respective table
-	SET
-		year_2016 = payment_type_2016_sum
+SET year_2016 = payment_type_2016_sum
 FROM (
 	SELECT
-		payment_type,
-		SUM(payment_type_2016) AS payment_type_2016_sum -- sum to get how many times a payment type was used
+	payment_type,
+	SUM(payment_type_2016) AS payment_type_2016_sum -- sum to get how many times a payment type was used
 	FROM (
 		SELECT
-			purchase_year,
-			payment_type,
-			CASE -- assign 1 for summing up later
-				WHEN payment_type = 'not_defined' THEN 1
-				WHEN payment_type = 'boleto' THEN 1
-				WHEN payment_type = 'debit_card' THEN 1
-				WHEN payment_type = 'voucher' THEN 1
-				WHEN payment_type = 'credit_card' THEN 1
-			END AS payment_type_2016
+		purchase_year,
+		payment_type,
+		CASE -- assign 1 for summing up later
+			WHEN payment_type = 'not_defined' THEN 1
+			WHEN payment_type = 'boleto' THEN 1
+			WHEN payment_type = 'debit_card' THEN 1
+			WHEN payment_type = 'voucher' THEN 1
+			WHEN payment_type = 'credit_card' THEN 1
+		END AS payment_type_2016
 		FROM (
-				SELECT
-					DATE_PART ('year', order_purchase_timestamp) AS purchase_year, -- year
-					payment_type -- payment type
-				FROM orders_dataset AS od
-				JOIN order_payments_dataset AS opd
-					ON od.order_id = opd.order_id
-			) AS t1
+			SELECT
+			DATE_PART ('year', order_purchase_timestamp) AS purchase_year, -- year
+			payment_type -- payment type
+			FROM orders_dataset AS od
+			JOIN order_payments_dataset AS opd
+			ON od.order_id = opd.order_id
+		) AS t1
 		WHERE purchase_year = 2016 -- filter for year 2016 only
-		) AS t2
+	) AS t2
 	GROUP BY payment_type -- aggregate to get the sum based on payment type
-	) AS t3
+) AS t3
 WHERE stage_4.payment_type = t3.payment_type;
 
 
 -- Year 2017
 
 UPDATE stage_4 -- insert the values into the respective table
-	SET
-		year_2017 = payment_type_2017_sum
+SET year_2017 = payment_type_2017_sum
 FROM (
 	SELECT
-		payment_type,
-		SUM(payment_type_2017) AS payment_type_2017_sum -- sum to get how many times a payment type was used
+	payment_type,
+	SUM(payment_type_2017) AS payment_type_2017_sum -- sum to get how many times a payment type was used
 	FROM (
 		SELECT
-			purchase_year,
-			payment_type,
-			CASE -- assign 1 for summing up later
-				WHEN payment_type = 'not_defined' THEN 1
-				WHEN payment_type = 'boleto' THEN 1
-				WHEN payment_type = 'debit_card' THEN 1
-				WHEN payment_type = 'voucher' THEN 1
-				WHEN payment_type = 'credit_card' THEN 1
+		purchase_year,
+		payment_type,
+		CASE -- assign 1 for summing up later
+			WHEN payment_type = 'not_defined' THEN 1
+			WHEN payment_type = 'boleto' THEN 1
+			WHEN payment_type = 'debit_card' THEN 1
+			WHEN payment_type = 'voucher' THEN 1
+			WHEN payment_type = 'credit_card' THEN 1
 			END AS payment_type_2017
 		FROM (
-				SELECT
-					DATE_PART ('year', order_purchase_timestamp) AS purchase_year, -- year
-					payment_type -- payment type
-				FROM orders_dataset AS od
-				JOIN order_payments_dataset AS opd
-					ON od.order_id = opd.order_id
-			) AS t1
+			SELECT
+			DATE_PART ('year', order_purchase_timestamp) AS purchase_year, -- year
+			payment_type -- payment type
+			FROM orders_dataset AS od
+			JOIN order_payments_dataset AS opd
+			ON od.order_id = opd.order_id
+		) AS t1
 		WHERE purchase_year = 2017 -- filter for year 2017 only
-		) AS t2
+	) AS t2
 	GROUP BY payment_type -- aggregate to get the sum based on payment type
-	) AS t3
+) AS t3
 WHERE stage_4.payment_type = t3.payment_type;
 
 
 -- Year 2018
 
 UPDATE stage_4 -- insert the values into the respective table
-	SET
-		year_2018 = payment_type_2018_sum
+SET year_2018 = payment_type_2018_sum
 FROM (
 	SELECT
-		payment_type,
-		SUM(payment_type_2018) AS payment_type_2018_sum -- sum to get how many times a payment type was used
+	payment_type,
+	SUM(payment_type_2018) AS payment_type_2018_sum -- sum to get how many times a payment type was used
 	FROM (
 		SELECT
-			purchase_year,
-			payment_type,
-			CASE -- assign 1 for summing up later
-				WHEN payment_type = 'not_defined' THEN 1
-				WHEN payment_type = 'boleto' THEN 1
-				WHEN payment_type = 'debit_card' THEN 1
-				WHEN payment_type = 'voucher' THEN 1
-				WHEN payment_type = 'credit_card' THEN 1
-			END AS payment_type_2018
+		purchase_year,
+		payment_type,
+		CASE -- assign 1 for summing up later
+			WHEN payment_type = 'not_defined' THEN 1
+			WHEN payment_type = 'boleto' THEN 1
+			WHEN payment_type = 'debit_card' THEN 1
+			WHEN payment_type = 'voucher' THEN 1
+			WHEN payment_type = 'credit_card' THEN 1
+		END AS payment_type_2018
 		FROM (
-				SELECT
-					DATE_PART ('year', order_purchase_timestamp) AS purchase_year, -- year
-					payment_type -- payment type
-				FROM orders_dataset AS od
-				JOIN order_payments_dataset AS opd
-					ON od.order_id = opd.order_id
-			) AS t1
+			SELECT
+			DATE_PART ('year', order_purchase_timestamp) AS purchase_year, -- year
+			payment_type -- payment type
+			FROM orders_dataset AS od
+			JOIN order_payments_dataset AS opd
+			ON od.order_id = opd.order_id
+		) AS t1
 		WHERE purchase_year = 2018 -- filter for year 2018 only
-		) AS t2
+	) AS t2
 	GROUP BY payment_type -- aggregate to get the sum based on payment type
-	) AS t3
+) AS t3
 WHERE stage_4.payment_type = t3.payment_type;
